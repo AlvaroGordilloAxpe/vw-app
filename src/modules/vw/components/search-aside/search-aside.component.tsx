3286a94f23c37cd5aaea760626c3d217f528779c
alvarogre <alvarogre@ext.stradivarius.com>
2023-12-21 00:43:15 +0100

    se han completado los cambios tras rev 1.1

--- src/modules/vw/components/search-aside/search-aside.component.tsx
+++ src/modules/vw/components/search-aside/search-aside.component.tsx
@@ -1,16 +1,17 @@
 import styles from './search-aside.module.css'
 import cn from 'classnames'
-import { useGetSearches } from '@/vw/services'
 import * as Command from '@/common/components/ui/command'
+import { Button } from '@/common/components/ui/button'
+import { Icons } from '@/common/components/icons'
 import { Search } from 'lucide-react'
 import { useState } from 'react'
 import { useVWContext } from '@/vw/contexts'
+import { useGetSearches } from '@/vw/services'
 
 export function VWSearchAsideComponent() {
     const { data, isLoading } = useGetSearches()
     const [selectedItem, setSelectedItem] = useState('')
     const { setSearchID } = useVWContext()
-    //const search = useCommandState((state) => state.search)
 
     return (
         <div
@@ -28,17 +29,31 @@ export function VWSearchAsideComponent() {
                         <Command.Empty>Sin resultados</Command.Empty>
                         <Command.Group>
                             {data.map((item) => (
-                                <Command.Item
-                                    data-clicked={item.name === selectedItem}
+                                <div
                                     key={item.id}
-                                    className={styles.items}
-                                    onSelect={(value) => {
-                                        setSelectedItem(value)
-                                        setSearchID(item.id)
-                                    }}
+                                    className="flex flex-row justify-between"
                                 >
-                                    {item.name}
-                                </Command.Item>
+                                    <Command.Item
+                                        data-clicked={
+                                            item.name === selectedItem
+                                        }
+                                        className={styles.items}
+                                        onSelect={(value) => {
+                                            setSelectedItem(value)
+                                            setSearchID(item.id)
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Command.Item>
+                                    <Button
+                                        className={styles.deleteIcon}
+                                        size="sm"
+                                        variant="link"
+                                        title={`Delete ${item.id}`}
+                                    >
+                                        <Icons.close />
+                                    </Button>
+                                </div>
                             ))}
                         </Command.Group>
                     </Command.List>
--- src/modules/vw/components/search-form/search-form.component.tsx
+++ src/modules/vw/components/search-form/search-form.component.tsx
@@ -5,14 +5,20 @@ import * as Form from '@/common/components/ui/form'
 import { LoadingButton } from '@/common/components/loading-button'
 import { Input } from '@/common/components/ui/input'
 import { Button } from '@/common/components/ui/button'
+import * as Dialog from '@/common/components/ui/dialog'
 
 const schema = z.object({
     id: z.string(),
     name: z.string(),
     testid: z.string(),
 })
+const schemaDialog = z.object({
+    titulo: z.string(),
+})
+const ROOT_SUBMIT = 'root.submit'
 
 type SearchFormDataType = z.infer<typeof schema>
+type SearchFormDialogDataType = z.infer<typeof schemaDialog>
 
 type SearchFormProps = Omit<
     React.HTMLAttributes<HTMLDivElement>,
@@ -35,7 +41,7 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
                 const response = await onSubmit(data)
 
                 if (!response) {
-                    form.setError('root.submit', {
+                    form.setError(ROOT_SUBMIT, {
                         type: 'server',
                         message: 'Network Error',
                     })
@@ -43,7 +49,36 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
             } catch (e: any) {
                 console.error('UploadForm.onSubmit error', e)
 
-                form.setError('root.submit', {
+                form.setError(ROOT_SUBMIT, {
+                    type: 'unknown',
+                    message: 'Unknown error',
+                })
+            }
+        },
+    })
+    const formDialog = Form.useZodForm<SearchFormDialogDataType>({
+        criteriaMode: 'firstError',
+        schema: schemaDialog,
+        defaultValues: {
+            titulo: '',
+        },
+        onSubmit: async (data) => {
+            try {
+                //const response = await onSubmit(data)
+                console.log('dataDialog', data)
+
+                /*
+                if (!response) {
+                    form.setError(ROOT_SUBMIT, {
+                        type: 'server',
+                        message: 'Network Error',
+                    })
+                }
+                */
+            } catch (e: any) {
+                console.error('UploadForm.onSubmit error', e)
+
+                form.setError(ROOT_SUBMIT, {
                     type: 'unknown',
                     message: 'Unknown error',
                 })
@@ -54,15 +89,15 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
     return (
         <div
             data-testid="vw-search-form-component"
-            className={cn(styles.container, 'space-y-8 pl-1 pr-1')}
+            className={cn(styles.container, 'flex flex-row')}
         >
-            <Form.Root {...form}>
+            <Form.Root {...form} className="basis-11/12">
                 <div className="flex flex-row gap-x-8">
                     <Form.Field
                         control={form.control}
                         name="id"
                         render={({ field }) => (
-                            <Form.Item className="auto">
+                            <Form.Item className="basis-1/3">
                                 <Form.Label>ID</Form.Label>
                                 <Form.Input>
                                     <Input type="text" {...field} />
@@ -75,7 +110,7 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
                         control={form.control}
                         name="name"
                         render={({ field }) => (
-                            <Form.Item className="auto">
+                            <Form.Item className="basis-1/3">
                                 <Form.Label>Name</Form.Label>
                                 <Form.Input>
                                     <Input type="text" {...field} />
@@ -88,7 +123,7 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
                         control={form.control}
                         name="testid"
                         render={({ field }) => (
-                            <Form.Item className="auto">
+                            <Form.Item className="basis-1/3">
                                 <Form.Label>Test ID</Form.Label>
                                 <Form.Input>
                                     <Input type="text" {...field} />
@@ -97,22 +132,63 @@ export function VWSearchFormComponent({ onSubmit, ...props }: SearchFormProps) {
                             </Form.Item>
                         )}
                     />
-                    <div className="basis-1/4 mt-8">
-                        <LoadingButton
-                            loading={form.formState.isSubmitting}
-                            type="submit"
-                        >
-                            Search
-                        </LoadingButton>
-                        <Button variant="secondary" className="ml-7">
-                            Save Search
-                        </Button>
-                    </div>
+                    <LoadingButton
+                        className="auto mr-6 mt-8"
+                        loading={form.formState.isSubmitting}
+                        type="submit"
+                    >
+                        Search
+                    </LoadingButton>
                 </div>
                 <Form.CustomMessage className="mt-8" isError>
                     {form.formState.errors.root?.submit?.message || null}
                 </Form.CustomMessage>
             </Form.Root>
+
+            <Dialog.Root>
+                <Dialog.Trigger asChild className="auto mt-8">
+                    <Button variant="secondary">Save Search</Button>
+                </Dialog.Trigger>
+                <Dialog.Modal className="bg-neutral-50 shadow-xl shadow-slate-900/30">
+                    <Dialog.Header>
+                        <Dialog.DialogTitle>
+                            Give a name to the search
+                        </Dialog.DialogTitle>
+                        <Dialog.Description>
+                            <Form.Root {...formDialog}>
+                                <div className="flex flex-row mt-4">
+                                    <Form.Field
+                                        control={formDialog.control}
+                                        name="titulo"
+                                        render={({ field }) => (
+                                            <Form.Item className="basis-11/12">
+                                                <Form.Input>
+                                                    <Input
+                                                        type="text"
+                                                        {...field}
+                                                        placeholder="nombre..."
+                                                        autoFocus
+                                                    />
+                                                </Form.Input>
+                                                <Form.Message />
+                                            </Form.Item>
+                                        )}
+                                    />
+                                    <LoadingButton
+                                        className="auto ml-4"
+                                        loading={
+                                            formDialog.formState.isSubmitting
+                                        }
+                                        type="submit"
+                                    >
+                                        Save
+                                    </LoadingButton>
+                                </div>
+                            </Form.Root>
+                        </Dialog.Description>
+                    </Dialog.Header>
+                </Dialog.Modal>
+            </Dialog.Root>
         </div>
     )
 }
